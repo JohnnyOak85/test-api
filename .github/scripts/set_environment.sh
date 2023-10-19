ae3f62d21d9fa013474b4b82f9ef8bc3ddc00bc1
JohnnyOak85 <joao.cmp@proton.me>
2023-10-19 12:13:21 +0100

    updating set-environment

--- .github/scripts/set_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set environment variables
-ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export HOST=${HOST}" >>~/.bashrc'
-ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export PORT=${PORT}" >>~/.bashrc'
-
-# Reload the shell to apply changes
-ssh $SSH_OPTIONS $EC2_INSTANCE 'source ~/.bashrc'
