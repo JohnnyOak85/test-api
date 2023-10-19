baf87fa9c7d70ba979dbcbcd095355f193d4575b
JohnnyOak85 <joao.cmp@proton.me>
2023-10-19 13:38:18 +0100

    refactoring environment setting

--- .github/scripts/set-environment.sh
+++ .github/scripts/set-environment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export HOST='${HOST}'" | tee ~/.profile'
-ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export PORT='${PORT}'" | tee -a ~/.profile'
+ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export HOST='${HOST}'" | tee ~/.env'
+ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export PORT='${PORT}'" | tee -a ~/.env'
