93b8eb4908f873c9bed4a96eee8fa5f92fc2bfa7
JohnnyOak85 <joao.cmp@proton.me>
2023-10-19 13:48:05 +0100

    correcting env file

--- .github/scripts/set-environment.sh
+++ .github/scripts/set-environment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export HOST='${HOST}'" | tee ~/.env'
-ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "export PORT='${PORT}'" | tee -a ~/.env'
+ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "HOST='${HOST}'" | tee ~/.env'
+ssh $SSH_OPTIONS $EC2_INSTANCE 'echo "PORT='${PORT}'" | tee -a ~/.env'
